607810efb53a4111df66bfa6a73d6888e257f5f0
Juliane Seidl <seidl5@hm.edu>
2017-04-25 22:05:31 +0200

    noch nichts implementiert

--- src/main/java/edu/hm/jularo/shareit/service/MediaServiceImpl.java
+++ src/main/java/edu/hm/jularo/shareit/service/MediaServiceImpl.java
@@ -1,4 +1,46 @@
 package edu.hm.jularo.shareit.service;
 
-public class MediaServiceImpl {
+import edu.hm.jularo.shareit.models.Book;
+import edu.hm.jularo.shareit.models.Disc;
+
+public class MediaServiceImpl implements MediaService {
+    @Override
+    public MediaServiceResult addBook(Book book) {
+        return null;
+    }
+
+    @Override
+    public MediaServiceResult addDisc(Disc disc) {
+        return null;
+    }
+
+    @Override
+    public Book getBook(String isbn) {
+        return null;
+    }
+
+    @Override
+    public Book[] getBooks() {
+        return new Book[0];
+    }
+
+    @Override
+    public Disc getDisc(String barcode) {
+        return null;
+    }
+
+    @Override
+    public Disc[] getDiscs() {
+        return new Disc[0];
+    }
+
+    @Override
+    public MediaServiceResult updateBook(Book book) {
+        return null;
+    }
+
+    @Override
+    public MediaServiceResult updateDisc(Disc disc) {
+        return null;
+    }
 }
